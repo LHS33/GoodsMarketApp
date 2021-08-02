b425c56c8d199399405664425a6f0cff9b7282cc
LHS33 <coconutii@naver.com>
2021-08-02 16:00:43 +0900

    등록&로그인 기능 구현 중.. 카테고리랑 DB 정도 남음 메인 쪽 담당분 intent 코드 추가 필요

--- app/src/main/java/com/example/goodsmarketapp/Login.kt
+++ app/src/main/java/com/example/goodsmarketapp/Login.kt
@@ -1,11 +1,72 @@
 package com.example.goodsmarketapp
 
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+    lateinit var nameEdit: EditText
+    lateinit var passwordEdit: EditText
+    lateinit var regButton: Button
+    lateinit var loginButton: Button
+    lateinit var noAccount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        nameEdit = findViewById(R.id.nameEdit)
+        passwordEdit = findViewById(R.id.passwordEdit)
+        regButton = findViewById(R.id.regButton)
+        loginButton = findViewById(R.id.loginButton)
+        noAccount = findViewById(R.id.noAccount)
+
+        inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "personnelDB", null, 1) {
+            override fun onCreate(db: SQLiteDatabase?) {
+
+            }
+
+            override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+            }
+        }
+
+
+
+        //회원가입 버튼 누름 -> 회원가입 페이지로
+        regButton.setOnClickListener {
+            var intent = Intent(this, PersonnelReg::class.java)
+            startActivity(intent)
+
+        }
+
+        //로그인 버튼 누름 -> 둘 다 맞으면 닉네임or실명님, 어서오세요 Toast 하고 메인으로, 틀리면 없는 이름이거나 비번입니다 Toast
+        loginButton.setOnClickListener {
+            if() {
+                if() { //둘 다 맞음. MainActivity 쪽에서 이름 비번 받아오는 코드 넣어야 합니다. (실전프로젝트 part4 6:30의 두 줄 참고)
+                    var intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("name", nameEdit.text.toString())
+                    intent.putExtra("password", passwordEdit.text.toString())
+                    Toast.makeText(this, "$nameEdit" + "님, 어서오세요.", Toast.LENGTH_SHORT).show()
+                    startActivity(intent)
+
+                } else { //이름은 맞는데 비번 틀림
+
+                    Toast.makeText(this, "없는 이름/비밀번호입니다.", Toast.LENGTH_SHORT).show()
+                }
+            } else { //이름부터 틀림
+
+                Toast.makeText(this, "없는 이름/비밀번호입니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/example/goodsmarketapp/ProductReg.kt
+++ app/src/main/java/com/example/goodsmarketapp/ProductReg.kt
@@ -1,20 +1,48 @@
 package com.example.goodsmarketapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 
 class ProductReg : AppCompatActivity() {
-
+    lateinit var camera: ImageView
+    lateinit var regProductButton: Button
+    lateinit var titleEdit: EditText
+    lateinit var category1: Spinner
+    lateinit var category2: Spinner
+    lateinit var minimumEdit: EditText
+    lateinit var fastEdit: EditText
+    lateinit var writeEdit: EditText
+    var list_of_1 = arrayOf("포토카드", "앨범", "공식 굿즈", "비공식 굿즈", "기타")
+    var list_of_2 = arrayOf("1일", "2일", "3일", "4일", "5일", "6일", "일주일")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_reg)
 
+        camera = findViewById(R.id.camera)
+        regProductButton = findViewById(R.id.regProductButton)
+        titleEdit = findViewById(R.id.titleEdit)
+        category1 = findViewById(R.id.category1)
+        category2 = findViewById(R.id.category2)
+        minimumEdit = findViewById(R.id.minimumEdit)
+        fastEdit = findViewById(R.id.fastEdit)
+        writeEdit = findViewById(R.id.writeEdit)
+
+        //상품 등록 버튼 누름 -> 글 등록하고 메인 화면으로 돌아감
+        regProductButton.setOnClickListener {
+            //글 등록하는 문구 아직
+
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+        //뭔가 해보려다 안 된 쓰레기 흔적
         var spinner : Spinner
         var sData = resources.getStringArray(R.array.my_array)
         var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sData)
